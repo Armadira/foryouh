180d2e2170f578580371246d45f39f461930adea
Armadira <nadiramarpaung@gmail.com>
2025-10-04 00:26:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const message = `hi, husni. i've run out of words to say, but thank you for surviving in this cruel world. a wish for you on your birthday, whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always. 
-love, dira`;
+  love, dira`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -22,6 +22,7 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
 
 
